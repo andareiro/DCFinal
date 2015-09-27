591cf0c0dbfbe9f25cd0dd77277b299922302b8e
Andareiro <moorehead@gmail.com>
2015-09-27 17:39:52 -0500

    Updated for more thorough variable recode

--- run_analysis.R
+++ run_analysis.R
@@ -41,30 +41,64 @@ activityLabels$activity <- tolower(activityLabels$activity)
 fullSet <- left_join(fullSet, activityLabels, by = c("label" = "id"))
 
 ## Create a data set of the mean and std deviation values for all the entries
-meanStdSet <- select(fullSet, subject, activity, contains("mean"), contains("std"))
+meanStdSet <- select(fullSet, subject, activity, contains("mean"), contains("std"),
+                     -contains("angle"))
 
-## Neaten up variable names
-
-varNames <- names(meanStdSet)
-varNames <- gsub("\\.{3}", "_", varNames)
-varNames <- gsub("\\..$", "", varNames)
-varNames <- gsub("\\.{1}", "_", varNames)
-varNames <- tolower(varNames)
-names(meanStdSet) <- varNames
-
-## meanStdSet is done!
-
-## Now go through a complex set of manipulations to get a tidy means
-## set ready for export
+## To make our data tidy, melt it so that we have a single colume for the test
+## values
 
 horizSet <- melt(meanStdSet, id.vars = c("subject", "activity"),
                  variable.name = c("test_var"), value.name = "test_val")
+
+## Go ahead and calculate the mean for all the variables
+
 horizSet <- horizSet %>%
   ungroup () %>%
   group_by(subject, activity, test_var) %>%
   dplyr::summarize(mean = mean(test_val))
 
-tidyMeans <- dcast(horizSet, subject + activity ~ test_var)
+## Final step to make our data tidy, recode the variable names into actual 
+## data fields, based on data in features_info.txt
+
+## Make the names lowercase and easier to handle
+horizSet$test_var <- tolower(horizSet$test_var)
+
+## Instrument type
+horizSet$instrument[grepl("acc",  horizSet$test_var) ] <- "accelerometer"
+horizSet$instrument[grepl("gyro",  horizSet$test_var) ] <- "gyroscope"
+
+## Signal domain type
+horizSet$signal[grepl("^f",  horizSet$test_var) ] <- "frequency"
+horizSet$signal[grepl("\\_f",  horizSet$test_var) ] <- "frequency"
+horizSet$signal[grepl("^t",  horizSet$test_var) ] <- "time"
+horizSet$signal[grepl("\\_t",  horizSet$test_var) ] <- "time"
+
+## Signal type
+horizSet$signal_type[grepl("body",  horizSet$test_var) ] <- "body"
+horizSet$signal_type[grepl("gravity",  horizSet$test_var) ] <- "gravity"
+
+## Jerk signal, yes or no
+horizSet$jerk[grepl("jerk",  horizSet$test_var) ] <- "jerk"
+
+## Calculation
+horizSet$calc[grepl("mag",  horizSet$test_var) ] <- "magnitude"
+
+## Summary statistic
+horizSet$statistic[grepl("mean",  horizSet$test_var) ] <- "mean"
+horizSet$statistic[grepl("meanfreq",  horizSet$test_var) ] <- "mean frequency"
+horizSet$statistic[grepl("std",  horizSet$test_var) ] <- "std dev"
+
+## Axis
+horizSet$axis[grepl("\\_x",  horizSet$test_var) ] <- "x"
+horizSet$axis[grepl("\\_y",  horizSet$test_var) ] <- "y"
+horizSet$axis[grepl("\\_z",  horizSet$test_var) ] <- "z"
+
+## Remove that hideous test variable
+
+horizSet$test_var <- NULL
+
+## Looks good - write out the table
+
 write.table(tidyMeans, "./tidy means - accel set.txt", row.names = F)
 
 ## Done!
